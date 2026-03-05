d82dbda2c083472c0b6c8b472646abe5c778291d
Anniballi-Andrea <andreanniballi@gmail.com>
2026-03-05 10:20:40 +0100

    finish work

--- src/App.java
+++ src/App.java
@@ -4,10 +4,63 @@ public class App {
     public static void main(String[] args) {
         int[] number = new int[] { 1, 2, 4, 6, 10 };
 
-        int i = 0;
         ElencoDiInteri elenco = new ElencoDiInteri(number);
 
-        // elenco.getElenco();
+        ElencoDiInteri secondoElenco = new ElencoDiInteri();
+
+        System.out.println(elenco.getElementoSuccessivo() + " sei arrivato alla fine? " + elenco.hasAncoraElementi());
+
+        System.out.println(elenco.getElementoSuccessivo() + " sei arrivato alla fine? " + elenco.hasAncoraElementi());
+
+        System.out.println(elenco.getElementoSuccessivo() + " sei arrivato alla fine? " + elenco.hasAncoraElementi());
+
+        System.out.println(elenco.getElementoSuccessivo() + " sei arrivato alla fine? " + elenco.hasAncoraElementi());
+
+        System.out.println(elenco.getElementoSuccessivo() + " sei arrivato alla fine? " + elenco.hasAncoraElementi());
+
+        System.out.println(elenco.getElementoSuccessivo() + " sei arrivato alla fine? " + elenco.hasAncoraElementi());
+
+        System.out.println(elenco.getElementoSuccessivo() + " sei arrivato alla fine? " + elenco.hasAncoraElementi());
+
+        System.out.println(elenco.getElementoSuccessivo() + " sei arrivato alla fine? " + elenco.hasAncoraElementi());
+
+        System.out.println(elenco.getElementoSuccessivo() + " sei arrivato alla fine? " + elenco.hasAncoraElementi());
+
+        System.out.println(elenco.getElementoSuccessivo() + " sei arrivato alla fine? " + elenco.hasAncoraElementi());
+
+        System.out.println(elenco.getElementoSuccessivo() + " sei arrivato alla fine? " + elenco.hasAncoraElementi());
+
+        System.out.println(elenco.getElementoSuccessivo() + " sei arrivato alla fine? " + elenco.hasAncoraElementi());
+
+        System.out.println(elenco.getElementoSuccessivo() + " sei arrivato alla fine? " + elenco.hasAncoraElementi());
+
+        System.out.println(elenco.getElementoSuccessivo() + " sei arrivato alla fine? " + elenco.hasAncoraElementi());
+
+        System.out.println(elenco.getElementoSuccessivo() + " sei arrivato alla fine? " + elenco.hasAncoraElementi());
+
+        System.out.println(elenco.getElementoSuccessivo() + " sei arrivato alla fine? " + elenco.hasAncoraElementi());
+
+        System.out.println(elenco.getElementoSuccessivo() + " sei arrivato alla fine? " + elenco.hasAncoraElementi());
+
+        System.out.println(elenco.getElementoSuccessivo() + " sei arrivato alla fine? " + elenco.hasAncoraElementi());
+
+        secondoElenco.addNumber(34);
+
+        secondoElenco.addNumber(3);
+
+        secondoElenco.addNumber(87);
+
+        secondoElenco.addNumber(6);
+
+        secondoElenco.addNumber(9);
+
+        secondoElenco.addNumber(94);
+
+        int i = 0;
+        while (!secondoElenco.hasAncoraElementi()) {
+            System.out.println(secondoElenco.getElementoSuccessivo());
+            i++;
+        }
 
     }
 }
--- src/classi/ElencoDiInteri.java
+++ src/classi/ElencoDiInteri.java
@@ -2,24 +2,52 @@ package classi;
 
 public class ElencoDiInteri {
     private int[] elenco;
+    private int count;
 
     public ElencoDiInteri(int[] number) {
         this.elenco = number;
+        this.count = -1;
+
+    }
+
+    public ElencoDiInteri() {
+        this.elenco = new int[0];
+        this.count = -1;
     }
 
     public void getElenco() {
         for (int i = 0; i < this.elenco.length; i++) {
-            System.out.println(this.elenco[i]);
+            System.out.println("il numero alla posizione " + i + " è: " + this.elenco[i]);
         }
     }
 
-    public int getElementoSuccessivo(int i) {
+    public int getElementoSuccessivo() {
+        this.count = count + 1;
 
-        if (i > this.elenco.length) {
-            i = 0;
-        }
-        System.out.println(this.elenco[i]);
-        return i = i++;
+        return this.elenco[count];
 
     }
+
+    public boolean hasAncoraElementi() {
+        if (this.count >= this.elenco.length - 1) {
+            this.count = -1;
+            return true;
+        } else {
+
+            return false;
+        }
+
+    }
+
+    public void addNumber(int number) {
+        int[] newElenco = new int[this.elenco.length + 1];
+        for (int i = 0; i < elenco.length; i++) {
+            newElenco[i] = this.elenco[i];
+
+        }
+        newElenco[this.elenco.length] = number;
+
+        this.elenco = newElenco;
+    }
+
 }
